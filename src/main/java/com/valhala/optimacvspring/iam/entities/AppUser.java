982d4d22521a961fb8d4e02a37c1a0fa9179fecd
chehachraf <achrafchehboun@outlook.com>
2026-03-09 14:05:11 +0000

    feat: Introduce `AppUser` entity and `Role` enum for Identity and Access Management.

--- src/main/java/com/valhala/optimacvspring/iam/entities/AppUser.java
+++ src/main/java/com/valhala/optimacvspring/iam/entities/AppUser.java
@@ -1,4 +1,28 @@
 package com.valhala.optimacvspring.iam.entities;
 
+
+import com.valhala.optimacvspring.iam.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "app_users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private Role role;
 }
