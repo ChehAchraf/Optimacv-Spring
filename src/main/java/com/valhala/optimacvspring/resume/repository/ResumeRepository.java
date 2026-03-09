9b7fc611fb9e46000594a4d847ebd8473040e375
chehachraf <achrafchehboun@outlook.com>
2026-03-09 14:25:27 +0000

    feat: create ResumeRepository for managing Resume entities and querying by user ID

---
+++ src/main/java/com/valhala/optimacvspring/resume/repository/ResumeRepository.java
@@ -0,0 +1,15 @@
+package com.valhala.optimacvspring.resume.repository;
+
+import com.valhala.optimacvspring.resume.entites.Resume;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ResumeRepository extends JpaRepository<Resume, UUID> {
+
+    List<Resume> findAllByUserId(UUID userId);
+
+}
