734a72067bb3c57aad65c9c8ecfdc3ba916edf7e
chehachraf <achrafchehboun@outlook.com>
2026-03-09 13:50:03 +0000

    init the project

---
+++ src/main/java/com/valhala/optimacvspring/OptimacvSpringApplication.java
@@ -0,0 +1,13 @@
+package com.valhala.optimacvspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimacvSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimacvSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valhala/optimacvspring/OptimacvSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valhala.optimacvspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimacvSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
