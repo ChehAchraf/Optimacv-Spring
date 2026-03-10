6325e8865df9892dab95f86e6d2214b85f51eb64
chehachraf <achrafchehboun@outlook.com>
2026-03-10 15:33:25 +0000

    feat: Add AuthMapper for mapping between RegisterRequestDTO and AppUser, and AppUser to AuthResponseDTO

---
+++ src/main/java/com/valhala/optimacvspring/iam/mapper/AuthMapper.java
@@ -0,0 +1,21 @@
+package com.valhala.optimacvspring.iam.mapper;
+
+import com.valhala.optimacvspring.iam.dto.AuthResponseDTO;
+import com.valhala.optimacvspring.iam.dto.RegisterRequestDTO;
+import com.valhala.optimacvspring.iam.entities.AppUser;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface AuthMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    AppUser toEntity(RegisterRequestDTO dto);
+
+    @Mapping(source = "user.email", target = "email")
+    @Mapping(source = "user.role", target = "role")
+    @Mapping(source = "token", target = "token")
+    AuthResponseDTO toAuthResponse(AppUser user, String token);
+}
