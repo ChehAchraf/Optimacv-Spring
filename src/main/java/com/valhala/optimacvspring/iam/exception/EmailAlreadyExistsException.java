e32aef1f78876d47915ef0b54619c762253c1dcb
chehachraf <achrafchehboun@outlook.com>
2026-03-10 15:33:19 +0000

    feat: Implement AuthService for user registration and login with email validation

---
+++ src/main/java/com/valhala/optimacvspring/iam/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.valhala.optimacvspring.iam.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+
+    public EmailAlreadyExistsException(String email) {
+        super("An account with email '" + email + "' already exists");
+    }
+}
