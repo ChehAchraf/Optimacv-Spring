e8e362f0ca010f4c5549e1e453578f61c51f8768
chehachraf <achrafchehboun@outlook.com>
2026-03-10 15:33:09 +0000

    feat: Add CustomUserDetailsService for user authentication by email

---
+++ src/main/java/com/valhala/optimacvspring/iam/service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.valhala.optimacvspring.iam.service;
+
+import com.valhala.optimacvspring.iam.entities.AppUser;
+import com.valhala.optimacvspring.iam.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        "User not found with email: " + email
+                ));
+    }
+}
