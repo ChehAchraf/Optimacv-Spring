024651551f8892e87a66d0a10c93e5131122ce7b
chehachraf <achrafchehboun@outlook.com>
2026-03-11 13:50:05 +0000

    feat: Create JobTargetRepository interface for JobTarget entity management

---
+++ src/main/java/com/valhala/optimacvspring/job/repository/JobTargetRepository.java
@@ -0,0 +1,11 @@
+package com.valhala.optimacvspring.job.repository;
+
+import com.valhala.optimacvspring.job.entities.JobTarget;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface JobTargetRepository extends JpaRepository<JobTarget, UUID> {
+}
