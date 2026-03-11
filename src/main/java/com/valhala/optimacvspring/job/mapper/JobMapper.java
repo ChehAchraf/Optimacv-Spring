26fd6eed7f95ccc2f636e0ceb97dbff70693a4b7
chehachraf <achrafchehboun@outlook.com>
2026-03-11 13:49:56 +0000

    feat: Implement JobMapper interface for mapping JobTarget to JobResponseDTO

---
+++ src/main/java/com/valhala/optimacvspring/job/mapper/JobMapper.java
@@ -0,0 +1,11 @@
+package com.valhala.optimacvspring.job.mapper;
+
+import com.valhala.optimacvspring.job.dto.JobResponseDTO;
+import com.valhala.optimacvspring.job.entities.JobTarget;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface JobMapper {
+
+    JobResponseDTO toResponseDTO(JobTarget jobTarget);
+}
