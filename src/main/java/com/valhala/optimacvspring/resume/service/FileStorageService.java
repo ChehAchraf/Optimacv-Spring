9628326d02e921600f25cd7cb64a0b308c6df177
chehachraf <achrafchehboun@outlook.com>
2026-03-11 12:50:25 +0000

    feat: Add WebConfig for resource handling and FileStorageService interface for file storage

---
+++ src/main/java/com/valhala/optimacvspring/resume/service/FileStorageService.java
@@ -0,0 +1,8 @@
+package com.valhala.optimacvspring.resume.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileStorageService {
+
+    String storeFile(MultipartFile file);
+}
