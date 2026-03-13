9cfb0d4f510974bf7b4b7fa63bf306acfa37dc60
chehachraf <achrafchehboun@outlook.com>
2026-03-13 12:27:17 +0000

    feat: update login functionality with error handling and signal for login error

--- front-end/src/core/model/user.model.ts
+++ front-end/src/core/model/user.model.ts
@@ -1,11 +1,17 @@
 
 export interface ILoginRequest {
-  email : string,
-  password : string,
+  email: string,
+  password: string,
 }
 
 export interface ILoginResponse {
-  token: string;
-  email: string;
-  role: "ROLE_USER" | "ROLE_ADMIN" | "ROLE_COMPANY";
+  token: string,
+  email: string,
+  role: "ROLE_USER" | "ROLE_ADMIN" | "ROLE_COMPANY",
+}
+
+export interface IRegisterRequest {
+  email: string,
+  password : string,
+  role: "ROLE_USER" | "ROLE_COMPANY",
 }
--- front-end/src/feature/login-page-component/login-page-component.ts
+++ front-end/src/feature/login-page-component/login-page-component.ts
@@ -1,6 +1,9 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import { RouterLink } from "@angular/router";
 import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../core/service/auth-service';
+import { ILoginRequest } from '../../core/model/user.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-page-component',
@@ -9,7 +12,10 @@ import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
   styleUrl: './login-page-component.css',
 })
 export class LoginPageComponent {
-  private fb = inject(FormBuilder)
+  private readonly authService = inject(AuthService)
+  private readonly fb = inject(FormBuilder)
+
+  loginError = signal<string>('');
 
 
   loginForm = this.fb.group({
@@ -18,8 +24,25 @@ export class LoginPageComponent {
   })
 
   onSubmit() : void{
-    if(this.loginForm.valid){
-      console.log("data : ", this.loginForm.value)
+    if (this.loginForm.valid) {
+      const formValue = this.loginForm.value;
+      
+      const request: ILoginRequest = {
+        email: formValue.email ?? '',
+        password: formValue.password ?? ''
+      };
+
+      this.authService.login(request).subscribe({
+        next : (data) =>{
+          console.log(data)
+        },
+        error : (error : HttpErrorResponse) =>{
+          this.loginError.set('password or email incorrect')
+        }
+      })
     }
   }
+
+
+
 }
