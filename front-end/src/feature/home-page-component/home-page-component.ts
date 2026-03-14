7ad3753964563308adf523173562a9b2768225bc
chehachraf <achrafchehboun@outlook.com>
2026-03-14 21:07:31 +0000

    feat: add additional sections to home page component

--- front-end/src/feature/home-page-component/home-page-component.ts
+++ front-end/src/feature/home-page-component/home-page-component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-import {HeroSectionComponent} from './components/hero-section-component/hero-section-component';
+import { HeroSectionComponent } from './components/hero-section-component/hero-section-component';
+import { ServiceSectionComponent } from './components/service-section-component/service-section-component';
+import {HowItWorksSection} from './components/how-it-works-section/how-it-works-section';
+import {PricingSectionComponent} from './components/pricing-section-component/pricing-section-component';
+import {TestimonialsSectionComponent} from './components/testimonials-section-component/testimonials-section-component';
+import {CtaSectionComponent} from './components/cta-section-component/cta-section-component';
 
 @Component({
   selector: 'app-home-page-component',
   imports: [
-    HeroSectionComponent
+    HeroSectionComponent,
+    ServiceSectionComponent,
+    HowItWorksSection,
+    PricingSectionComponent,
+    TestimonialsSectionComponent,
+    CtaSectionComponent
   ],
   templateUrl: './home-page-component.html',
   styleUrl: './home-page-component.css',
