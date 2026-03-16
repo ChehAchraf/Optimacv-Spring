01c98cf0f9d976dc4353257c607f3c987354bb83
chehachraf <achrafchehboun@outlook.com>
2026-03-16 11:22:45 +0000

    feat: add User Overview Component with template and styles

---
+++ front-end/src/feature/dashboard-component/pages/overview-page/user-overview-component/user-overview-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-overview-component',
+  imports: [],
+  templateUrl: './user-overview-component.html',
+  styleUrl: './user-overview-component.css',
+})
+export class UserOverviewComponent {}
