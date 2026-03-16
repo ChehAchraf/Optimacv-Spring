5e78c54093c54dfe5dbaa10e2f2302200954858c
chehachraf <achrafchehboun@outlook.com>
2026-03-16 11:06:14 +0000

    feat: add Analyze Resume page with HTML, CSS, and TypeScript files

---
+++ front-end/src/feature/dashboard-component/pages/analyze-resume-page/analyze-resume-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analyze-resume-page',
+  imports: [],
+  templateUrl: './analyze-resume-page.html',
+  styleUrl: './analyze-resume-page.css',
+})
+export class AnalyzeResumePage {}
---
+++ front-end/src/feature/dashboard-component/pages/bulk-rank-page/bulk-rank-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bulk-rank-page',
+  imports: [],
+  templateUrl: './bulk-rank-page.html',
+  styleUrl: './bulk-rank-page.css',
+})
+export class BulkRankPage {}
