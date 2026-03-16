f1e211938dbbf00254fdf7e8ce2c03f77e868cf9
chehachraf <achrafchehboun@outlook.com>
2026-03-16 10:15:31 +0000

    feat: add sidebar component with navigation links and close functionality

---
+++ front-end/src/feature/dashboard-component/components/side-bar-component/side-bar-component.ts
@@ -0,0 +1,36 @@
+import {Component, inject, input, output} from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import {
+  LucideAngularModule,
+  LayoutDashboard,
+  Users,
+  History,
+  Settings,
+  X,
+  Sparkles,
+} from 'lucide-angular';
+import {AuthStore} from '../../../../core/store/auth.store';
+
+@Component({
+  selector: 'app-side-bar-component',
+  standalone: true,
+  imports: [LucideAngularModule, RouterLink, RouterLinkActive],
+  templateUrl: './side-bar-component.html',
+  styleUrl: './side-bar-component.css',
+})
+export class SideBarComponent {
+  isOpen = input<boolean>(false);
+  protected readonly authStore = inject(AuthStore)
+  close = output<void>();
+
+  readonly LayoutDashboardIcon = LayoutDashboard;
+  readonly UsersIcon = Users;
+  readonly HistoryIcon = History;
+  readonly SettingsIcon = Settings;
+  readonly XIcon = X;
+  readonly SparklesIcon = Sparkles;
+
+  onClose(): void {
+    this.close.emit();
+  }
+}
