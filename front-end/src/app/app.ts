644c85ea4229d4e37c43edf5ca70cfb32d42c477
chehachraf <achrafchehboun@outlook.com>
2026-03-16 10:16:18 +0000

    feat: add loading spinner functionality and authentication guard

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,13 +1,27 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NavbarComponent} from '../shared/components/navbar-component/navbar-component';
+import {NgxSonnerToaster } from 'ngx-sonner';
+import {ToastComponent} from '../shared/components/toast-component/toast-component';
+import {NgxSpinner, NgxSpinnerComponent, NgxSpinnerService} from 'ngx-spinner';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, ToastComponent, NgxSonnerToaster, ToastComponent, ToastComponent, ToastComponent, NgxSpinnerComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
+
   protected readonly title = signal('front-end');
+  private spinner = inject(NgxSpinnerService)
+
+  ngOnInit(): void {
+    this.spinner.show()
+
+    setInterval(()=>{
+      this.spinner.hide()
+    },3000)
+  }
+
 }
