1bc7cfff40bf4e07a45e5bad0097b7a14eacf7b2
chehachraf <achrafchehboun@outlook.com>
2026-03-16 10:14:41 +0000

    feat: implement hero section component with responsive design and AI features

--- front-end/src/feature/home-page-component/components/hero-section-component/hero-section-component.ts
+++ front-end/src/feature/home-page-component/components/hero-section-component/hero-section-component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {
+  LucideAngularModule,
+  Sparkles,
+  ArrowRight,
+  PlayCircle,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-hero-section-component',
-  imports: [],
+  standalone: true,
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: './hero-section-component.html',
   styleUrl: './hero-section-component.css',
 })
-export class HeroSectionComponent {}
+export class HeroSectionComponent {
+  readonly SparklesIcon = Sparkles;
+  readonly ArrowRightIcon = ArrowRight;
+  readonly PlayCircleIcon = PlayCircle;
+}
