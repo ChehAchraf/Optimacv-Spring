11c6e3bdc96bbb53b38dd324ef53d75cc7eb6d74
chehachraf <achrafchehboun@outlook.com>
2026-03-17 10:32:41 +0000

    feat: update UserOverviewComponent to include CommonModule and LucideAngularModule imports

--- front-end/src/feature/dashboard-component/pages/overview-page/user-overview-component/user-overview-component.ts
+++ front-end/src/feature/dashboard-component/pages/overview-page/user-overview-component/user-overview-component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  LucideAngularModule,
+  LayoutDashboard,
+  Target,
+  FileText,
+  Activity,
+  Plus,
+  Search,
+} from 'lucide-angular'
 
 @Component({
   selector: 'app-user-overview-component',
-  imports: [],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './user-overview-component.html',
   styleUrl: './user-overview-component.css',
 })
