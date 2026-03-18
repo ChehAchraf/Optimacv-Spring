3c3153a614ff4436147abb26f8b1a15a8a5f3297
chehachraf <achrafchehboun@outlook.com>
2026-03-18 20:44:30 +0000

    feat: add analysis details component with HTML, CSS, and TypeScript files

---
+++ front-end/src/feature/dashboard-component/pages/analysis-details-component/analysis-details-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analysis-details-component',
+  imports: [],
+  templateUrl: './analysis-details-component.html',
+  styleUrl: './analysis-details-component.css',
+})
+export class AnalysisDetailsComponent {}
