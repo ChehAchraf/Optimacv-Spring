8102f1f5c2db38e6a830b3c122f7a062308f9591
chehachraf <achrafchehboun@outlook.com>
2026-03-18 22:40:01 +0000

    feat: add keywords card component with HTML, CSS, and TypeScript files

---
+++ front-end/src/feature/dashboard-component/pages/analysis-details-component/components/keywords-card/keywords-card-component.ts
@@ -0,0 +1,17 @@
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule, Search } from 'lucide-angular';
+
+@Component({
+  selector: 'app-keywords-card',
+  standalone: true,
+  imports: [CommonModule, LucideAngularModule],
+  templateUrl: './keywords-card-component.html',
+})
+export class KeywordsCardComponent {
+  matchingSkills = input.required<string[]>();
+  missingKeywords = input.required<string[]>();
+
+  readonly SearchIcon = Search;
+}
+
